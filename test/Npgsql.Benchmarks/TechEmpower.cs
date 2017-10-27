919c5803771d9953e81962d3190a16c8ec7623ec
Shay Rojansky <roji@roji.org>
2017-10-27 06:08:24 +0300

    Add benchmark for repeated prepare
    As part of TechEmpower optimization work

---
+++ test/Npgsql.Benchmarks/TechEmpower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Npgsql.Benchmarks
+{
+    [Config("columns=OperationPerSecond")]
+    [MemoryDiagnoser]
+    public class TechEmpower
+    {
+        public long Sum;
+
+        [Benchmark]
+        public void InstantiatePrepareExecuteParse()
+        {
+            using (var conn = BenchmarkEnvironment.OpenConnection())
+            using (var cmd = new NpgsqlCommand("SELECT 8, 'hello'", conn))
+            {
+                cmd.Prepare();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        unchecked
+                        {
+                            Sum += reader.GetInt32(0);
+                            Sum += reader.GetString(1).Length;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
