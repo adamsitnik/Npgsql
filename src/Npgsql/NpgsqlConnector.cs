3777ae126f309f88b60be41c15e10c49da57fdbf
Shay Rojansky <roji@roji.org>
2018-02-03 20:52:17 -0800

    Reimplement connection pool as lock-free
    Also corrected some inaccurate performance counter updating.
    
    TODO: Resolve race condition where a waiting open attempt blocks on its
    TaskCompletionSource as a connection is being inserted into the
    idle list. If another release doesn't come along we have an
    awaiter timing out although there's an idle connector...

--- src/Npgsql/NpgsqlConnector.cs
+++ src/Npgsql/NpgsqlConnector.cs
@@ -433,6 +433,7 @@ namespace Npgsql
 
                 if (Settings.Pooling && SupportsDiscard)
                     GenerateResetMessage();
+                Counters.NumberOfNonPooledConnections.Increment();
                 Counters.HardConnectsPerSecond.Increment();
                 Log.Trace($"Opened connection to {Host}:{Port}");
             }
@@ -1481,6 +1482,11 @@ namespace Npgsql
         /// (e.g. prepared statements), resetting parameters to their defaults, and resetting client-side
         /// state
         /// </summary>
+        /// <remarks>
+        /// It's important that this method be idempotent, since some race conditions in the pool
+        /// can cause it to be called twice (and also the user may close the connection right after
+        /// allocating it, without doing anything).
+        /// </remarks>
         internal void Reset()
         {
             Debug.Assert(State == ConnectorState.Ready);
