77e8a00ec50c8dfda272016f5ee39eab14c0a364
Shay Rojansky <roji@roji.org>
2018-06-12 18:39:53 +0100

    Benchmark for different methods of reading/writing strings

---
+++ test/Npgsql.Benchmarks/ReadString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Npgsql.Benchmarks
+{
+    public class ReadString
+    {
+        const int StringSize = 256;
+        byte[] Buffer = new byte[StringSize];
+        readonly Encoding Encoding = Encoding.UTF8;
+
+        public ReadString()
+        {
+            var data = new string('x', StringSize);
+            Encoding.GetBytes(data, 0, StringSize, Buffer, 0);
+        }
+
+        [Benchmark(Baseline = true)]
+        public string Array() => Encoding.GetString(Buffer, 0, StringSize);
+
+        [Benchmark]
+        public unsafe string SpanLegacy()
+        {
+            var span = new Span<byte>(Buffer);
+            fixed (byte* bytes = &span.GetPinnableReference())
+            {
+                return Encoding.GetString(bytes, StringSize);
+            }
+        }
+
+#if NETCOREAPP2_1
+        [Benchmark]
+        public string Span21()
+        {
+            var span = new Span<byte>(Buffer);
+            return Encoding.GetString(span);
+        }
+#endif
+    }
+}
---
+++ test/Npgsql.Benchmarks/WriteString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Npgsql.Benchmarks
+{
+    public class WriteString
+    {
+        const int StringSize = 256;
+        string Data = new string('x', StringSize);
+        byte[] Buffer = new byte[1024];
+
+        readonly Encoding Encoding = Encoding.UTF8;
+
+        [Benchmark(Baseline = true)]
+        public void Array() => Encoding.GetBytes(Data, 0, 256, Buffer, 0);
+
+        [Benchmark]
+        public unsafe void SpanLegacy()
+        {
+            var span = new Span<byte>(Buffer);
+            fixed (char* chars = Data)
+            fixed (byte* bytes = &span.GetPinnableReference())
+            {
+                Encoding.GetBytes(chars, StringSize, bytes, 1024);
+            }
+        }
+
+#if NETCOREAPP2_1
+        [Benchmark]
+        public void Span21()
+        {
+            var span = new Span<byte>(Buffer);
+            Encoding.GetBytes(Data, span);
+        }
+#endif
+    }
+}
