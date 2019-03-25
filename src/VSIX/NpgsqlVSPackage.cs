3e01eb12380c546db809231b493e478f50892f39
Shay Rojansky <roji@roji.org>
2019-03-25 14:29:05 +0100

    WIP

--- src/VSIX/NpgsqlVSPackage.cs
+++ src/VSIX/NpgsqlVSPackage.cs
@@ -1,17 +1,8 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="NpgsqlVSPackage.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
-using System;
-using System.ComponentModel.Design;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics.CodeAnalysis;
+﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
 
 namespace Npgsql.VSIX
 {
@@ -32,7 +23,7 @@ namespace Npgsql.VSIX
     /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     /// </para>
     /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideService(typeof(NpgsqlProviderObjectFactory), ServiceName = "PostgreSQL Provider Object Factory")]
@@ -40,37 +31,25 @@ namespace Npgsql.VSIX
     [NpgsqlProviderRegistration]
     [Guid(PackageGuidString)]
     [ProvideAutoLoad(UIContextGuids80.DataSourceWindowAutoVisible), ProvideAutoLoad(UIContextGuids80.DataSourceWindowSupported)]
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class NpgsqlVSPackage : Package
+    public sealed class VSIX2Package : AsyncPackage
     {
         /// <summary>
         /// NpgsqlVSPackage GUID string.
         /// </summary>
         public const string PackageGuidString = "ef991dc4-3119-4ed6-bdb3-c160ca562560";
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NpgsqlVSPackage"/> class.
-        /// </summary>
-        public NpgsqlVSPackage()
-        {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
-        }
+        const string NpgsqlInvariantName = "Npgsql";
+
+        #region Package Members
 
         /// <summary>
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
-        protected override void Initialize()
-        {
-            ((IServiceContainer)this).AddService(typeof(NpgsqlProviderObjectFactory), new NpgsqlProviderObjectFactory(), true);
-            SetupNpgsqlProviderFactory();
-            base.Initialize();
-        }
-
-        void SetupNpgsqlProviderFactory()
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             if (!(ConfigurationManager.GetSection("system.data") is DataSet systemData))
                 throw new Exception("No system.data section found in configuration manager!");
@@ -81,7 +60,7 @@ namespace Npgsql.VSIX
             else
             {
                 factoriesTable = systemData.Tables[systemData.Tables.IndexOf("DbProviderFactories")];
-                if (factoriesTable.Rows.Find(Constants.NpgsqlInvariantName) != null)
+                if (factoriesTable.Rows.Find(NpgsqlInvariantName) != null)
                 {
                     // There's already an entry for Npgsql in the machines.config.
                     // This should mean there's also a GAC-installed Npgsql - we don't need to do anything.
@@ -92,6 +71,12 @@ namespace Npgsql.VSIX
             // Add an entry for Npgsql
             factoriesTable.Rows.Add("Npgsql Data Provider", ".NET Data Provider for PostgreSQL",
                 Constants.NpgsqlInvariantName, "Npgsql.NpgsqlFactory, Npgsql");
+
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
         }
+
+        #endregion
     }
 }
